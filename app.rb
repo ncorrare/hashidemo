d0f7c7310f335427fb00816815c6a02bfc4f3712
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-31 00:27:08 +0000

    Initial commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+require 'fileutils'
+require 'tempfile'
+require 'net/http'
+require 'yaml'
+require 'vault'
+
+get '/' do
+  "Service discovery:"
+  consul = Resolv::DNS.new(:nameserver_port => [['localhost',8600]],
+           :ndots => 1)
+  memcacheaddr = consul.getaddress('blogs.memcached.service.consul.').to_s
+  "Memcache is running on " + memcacheaddr
+  mysqladdr = consul.getaddress('blogs.mysql.service.consul.').to_s
+  "MySQL is running on " + mysqladdr
+  vaultaddr = consul.getaddress('production.vault.service.consul.').to_s
+  "Vault is running on " + vaultaddr
+  vaulttoken = File.read '/etc/vaulttoken'
+  vault = Vault.configure do |config|
+    config.address = "https://"+ vaultaddr +":8200"
+    config.token = vaulttoken
+    config.ssl_verify = false
+  end
+
+  mysqlsecret = vault.logical.read("mysql/creds/readonly")
+end
