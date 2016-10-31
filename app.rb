a8b392a3c9fe8e59166ad3237fa369d2b2ac193a
Nicolas Corrarello <ncorrare@gmail.com>
2016-10-31 00:56:58 +0000

    Back to sinatra basics

--- app.rb
+++ app.rb
@@ -8,21 +8,22 @@ require 'resolv'
 
 
 get '/' do
-  'Service discovery:'
   consul = Resolv::DNS.new(:nameserver_port => [['localhost',8600]],
            :ndots => 1)
   memcacheaddr = consul.getaddress('blogs.memcached.service.consul.').to_s
-  "Memcache is running on #{memcacheaddr}"
   mysqladdr = consul.getaddress('blogs.mysql.service.consul.').to_s
-  "MySQL is running on #{mysqladdr}"
   vaultaddr = consul.getaddress('production.vault.service.consul.').to_s
-  "Vault is running on #{vaultaddr}"
   vaulttoken = File.read '/etc/vaulttoken'
   Vault.configure do |config|
     config.address = "https://"+ vaultaddr +":8200"
     config.token = vaulttoken
     config.ssl_verify = false
   end
+
+  "Service discovery:
+  Memcache is running on #{memcacheaddr}
+  MySQL is running on #{mysqladdr}
+  Vault is running on #{vaultaddr}"
 
   #mysqlsecret = Vault.logical.read("mysql/creds/readonly")
 end
