d64cacfa6c01730b965e1628624d7f7b69ea612a
Nicolas Corrarello <ncorrare@gmail.com>
2016-11-01 10:31:57 +0000

    Variables are created at startup, not get /

--- app.rb
+++ app.rb
@@ -6,22 +6,25 @@ require 'yaml'
 require 'vault'
 require 'resolv'
 
+consul = Resolv::DNS.new(:nameserver_port => [['localhost',8600]],
+           :ndots => 1)
+memcacheaddr = consul.getaddress('blogs.memcached.service.consul.').to_s
+mysqladdr = consul.getaddress('blogs.mysql.service.consul.').to_s
+vaultaddr = consul.getaddress('production.vault.service.consul.').to_s
+vaulttokenfile = File.read '/etc/vaulttoken'
+vaulttoken = vaulttokenfile.tr("\n","")
+localvault = Vault::Client.new(address: "https://#{vaultaddr}:8200", token: vaulttoken, ssl_verify: false)
+mysqlsecret = localvault.logical.read("mysql/creds/readonly")
+
 
 get '/' do
-  consul = Resolv::DNS.new(:nameserver_port => [['localhost',8600]],
-           :ndots => 1)
-  memcacheaddr = consul.getaddress('blogs.memcached.service.consul.').to_s
-  mysqladdr = consul.getaddress('blogs.mysql.service.consul.').to_s
-  vaultaddr = consul.getaddress('production.vault.service.consul.').to_s
-  vaulttokenfile = File.read '/etc/vaulttoken'
-  vaulttoken = vaulttokenfile.tr("\n","")
-  localvault = Vault::Client.new(address: "https://#{vaultaddr}:8200", token: vaulttoken, ssl_verify: false)
-  mysqlsecret = localvault.logical.read("mysql/creds/readonly")
-
-  "Service discovery:
-  Memcache is running on #{memcacheaddr}
-  MySQL is running on #{mysqladdr}, Username is #{mysqlsecret.data[:username]}
-  Vault is running on #{vaultaddr}"
+  "Service discovery:\n
+  Memcache is running on #{memcacheaddr}\n
+  MySQL is running on #{mysqladdr},\n 
+  Vault is running on #{vaultaddr}\n
+  Credentials: \n
+  Username is #{mysqlsecret.data[:username]}
+  "
 
 end
 
