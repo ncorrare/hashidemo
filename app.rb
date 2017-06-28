89466b95f97db77f033c12a1c5fc4afcc49388dc
Nicolas Corrarello <nicolas@corrarello.com>
2017-06-28 09:33:52 +0100

    Simplified example for demos

--- app.rb
+++ app.rb
@@ -5,22 +5,13 @@ require 'net/http'
 require 'yaml'
 require 'vault'
 require 'resolv'
-require 'mysql2'
 
 consul = Resolv::DNS.new(:nameserver_port => [['localhost',8600]],
            :ndots => 1)
-memcacheaddr = consul.getaddress('blogs.memcached.service.consul.').to_s
-mysqladdr = consul.getaddress('blogs.mysql.service.consul.').to_s
 vaultaddr = consul.getaddress('vault.service.consul.').to_s
-vaulttokenfile = File.read '/etc/vaulttoken'
-vaulttoken = vaulttokenfile.tr("\n","")
-localvault = Vault::Client.new(address: "https://#{vaultaddr}:8200", token: vaulttoken, ssl_verify: false)
-def getmysqlcreds(localvault)
-  mysqlsecret = localvault.logical.read("mysql/creds/readonly")
-  return mysqlsecret
-end
-
-mysqlvars = getmysqlcreds(localvault)
+vaulttoken = ENV["VAULT_TOKEN"]
+vault = Vault::Client.new(address: "https://#{vaultaddr}:8200", token: vaulttoken, ssl_verify: false)
+secret = localvault.logical.read("secret/hello")
 
 get '/' do
   begin
@@ -32,7 +23,7 @@ get '/' do
     client = Mysql2::Client.new(:host => mysqladdr, :username => mysqlvars.data[:username], :password => mysqlvars.data[:password])
     mysqlstatus = client.query("SHOW STATUS")
   end
-  erb :default, :locals => {:mysql => mysqladdr, :mysqluser => mysqlvars.data[:username], :memcache => memcacheaddr, :vault => vaultaddr, :ary => mysqlstatus}
+  erb :default, :locals => {:secret => secret.data[:key], :vault => vaultaddr}
 
 end
 
