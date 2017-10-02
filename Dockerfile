9ad8c7f911875e3b90d017611cb456a53fff5082
Nicolas Corrarello <nicolas@corrarello.com>
2017-10-02 13:15:30 +0100

    Rewrote for AWS ECS example

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,5 @@
 FROM ruby:2.1-onbuild
+RUN "curl -o /tmp/vault.zip https://releases.hashicorp.com/vault/0.8.1/vault_0.8.1_linux_amd64.zip"
+RUN "unzip /tmp/vault.zip -d /bin"
+ENTRYPOINT ["vault auth -token-only -method=aws role=test header_value=vault.example.com > /tmp/vault-token"]
 CMD ["./app.rb"]
--- app.rb
+++ app.rb
@@ -6,17 +6,40 @@ require 'yaml'
 require 'vault'
 require 'resolv'
 
-consul = Resolv::DNS.new(:nameserver_port => [['172.17.0.1',8600]],
-           :ndots => 1)
-vaultaddr = consul.getaddress('vault.service.consul.').to_s
-vaulttoken = ENV["VAULT_TOKEN"]
-vault = Vault::Client.new(address: "https://#{vaultaddr}:8200", token: vaulttoken, ssl_verify: false)
+if (defined?(ENV["VAULT_ADDR"]))
+  vaultaddr = ENV["VAULT_ADDR"]
+else
+#Attempt to discover it through Consul
+  consul = Resolv::DNS.new(:nameserver_port => [['127.0.0.1',8600]],
+             :ndots => 1)
+  vaulthost = consul.getaddress('vault.service.consul.').to_s
+  vaultaddr = "https://#{vaulthost}:8200"
+end
+if (defined?(ENV["VAULT_TOKEN"]))
+  vaulttoken = ENV["VAULT_TOKEN"]
+else
+#No one gave me a token, I'll try to find one
+  vaulttoken = File.read("/tmp/vault-token")
+end
+if (defined?(ENV["VAULT_SKIP_VERIFY"]))
+  ssl_verify = false
+else
+  ssl_verify = true
+end
+
+vault = Vault::Client.new(address: vaultaddr, token: vaulttoken, ssl_verify: false)
 secret = localvault.logical.read("secret/hello")
 
 get '/' do
   erb :default, :locals => {:secret => secret.data[:key], :vault => vaultaddr}
-
 end
+
+get '/secret/:id' do
+  secretpath = params[:id]
+  secret = localvault.logical.read("secret/#{secretpath}")
+  erb :default, :locals => {:secret => secret.data[:key], :vault => vaultaddr}
+end
+
 
 get '/test' do
   "Yeah, I'm up"
