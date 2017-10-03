0c79fd23781cf336579294f38d9bf47236f8399d
Nicolas Corrarello <nicolas@corrarello.com>
2017-10-03 22:07:44 +0100

    Setting VAULT_TOKEN as env var

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 if $(vault auth -token-only -method=aws role=test header_value=vault.example.com > /tmp/vault-token); then
   echo "Vault Token Obtained"
   cat /tmp/vault-token
+  export VAULT_TOKEN=$(cat /tmp/vault-token)
 else
   echo "Couldn't get Vault token"
 fi
