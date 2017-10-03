37eff98731071f1adefe4b8261b96e201f4e72a0
Nicolas Corrarello <nicolas@corrarello.com>
2017-10-03 16:37:52 +0100

    Confirming that the container is getting a Vault Token

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-vault auth -token-only -method=aws role=test header_value=vault.example.com > /tmp/vault-token
+if $(vault auth -token-only -method=aws role=test header_value=vault.example.com > /tmp/vault-token); then
+  echo "Vault Token Obtained"
+else
+  echo "Couldn't get Vault token"
+fi
 exec ruby /usr/src/app/app.rb
